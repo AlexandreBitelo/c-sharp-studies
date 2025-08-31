32033e85fa17824222e3d31a315ebd394be323e4
Alexandre Bitelo <alexandrebitelo41@gmail.com>
2025-08-31 20:23:38 -0300

    class about operators

---
+++ Aulas/Aula05/Aula05.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Aula05
+{
+    public static void Main()
+    {
+        /*
+        int num = 10;
+
+        // Operações Semelhantes
+        // num = num + 1; num += 1; num++;
+        num *= 2; // num = num+2  = - / *
+
+        Console.WriteLine(num);
+        */
+
+
+
+        // & = AND / e
+        // | = OR
+
+        bool res = (5 < 7) && (10 > 5); 
+        
+
+
+
+        Console.WriteLine(res);
+
+    }
+}

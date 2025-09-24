3161c6497a8b4fcea3df3ffe5808a35d188f292c
Alexandre Bitelo <alexandrebitelo41@gmail.com>
2025-09-23 22:08:43 -0300

    Exercises 4 and 5

---
+++ Exercicios/Exercícios Lógica de Programação/ex004/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+4) Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório 
+entre eles. 
+Ex: 
+Digite um valor: 8 
+Digite outro valor: 5 
+A soma entre 8 e 5 é igual a 13.
+*/
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int number1;
+        int number2;
+
+        Console.Write("Digite um valor: ");
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro valor: ");
+        number2 = int.Parse(Console.ReadLine());
+
+        Console.Write("A soma entre {0} e {1} é igual a {2}", number1, number2, (number1 + number2));
+    }
+}
---
+++ Exercicios/Exercícios Lógica de Programação/ex005/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+5) Faça um programa que leia as duas notas de um aluno em uma matéria e mostre 
+na tela a sua média na disciplina. 
+Ex:  
+Nota 1: 4.5 
+Nota 2: 8.5 
+A média entre 4.5 e 8.5 é igual a 6.5 
+*/
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        double grade1, grade2, average; 
+
+        Console.Write("Nota 1: ");
+        grade1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nota 2: ");
+        grade2 = Convert.ToDouble(Console.ReadLine());
+
+        average = ((grade1 + grade2) / 2);
+
+        Console.WriteLine("A média entre {0} e {1} é igual a {2}", grade1, grade2, average);
+    }
+}

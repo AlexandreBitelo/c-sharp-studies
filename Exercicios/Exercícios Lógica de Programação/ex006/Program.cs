3a125ffc419b99a4c2b72e7223fec3ba3ae52789
Alexandre Bitelo <alexandrebitelo41@gmail.com>
2025-10-10 19:01:38 -0300

    some exercises

---
+++ Exercicios/Exercícios Lógica de Programação/ex006/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+6) Faça um programa que leia um número inteiro e mostre o seu antecessor e seu 
+sucessor. 
+Ex:  
+Digite um número: 9 
+O antecessor de 9 é 8 
+O sucessor de 9 é 10 
+*/
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int number;
+
+        Console.Write("Digite um número: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("O antecessor de {0} é {1}", number, number - 1);
+        Console.WriteLine("O sucessor de {0} é {1}", number, number + 1);
+    }
+}
+
+
+
+
---
+++ Exercicios/Exercícios Lógica de Programação/ex007/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+7) Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a 
+sua terça parte. 
+Ex:  
+Digite um número: 3.5 
+O dobro de 3.5 é 7.0 
+A terça parte de 3.5 é 1.16666 
+*/
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        double number;
+
+        Console.WriteLine("Digite um número: ");
+        number = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("O dobro de {0} é {1}", number, number * 2);
+        Console.WriteLine("A terça parte de {0} é {1}", number, number / 3);
+
+
+    }
+}
+
+
+
+
+
+
+

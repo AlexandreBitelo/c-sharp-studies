3bf14a14fe813488681291f23c1603d0ff773f51
Alexandre Bitelo <alexandrebitelo41@gmail.com>
2025-10-25 13:00:03 -0300

    simple structure exercises

---
+++ Exercicios/Exercícios Lógica de Programação/ex015/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+15) Crie um programa que leia o número de dias trabalhados em um mês e mostre o 
+salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25 
+por hora trabalhada.
+*/
+
+using System; 
+
+class Program
+{
+    public static void Main()
+    {
+        int numberOfWorkedDays;
+        double finalPayment, payPerHour = 25;
+
+        Console.WriteLine("-- Calculadora de salário por hora --");
+
+        Console.Write("Dias do mês trabalhados: ");
+        numberOfWorkedDays = Convert.ToInt32(Console.ReadLine());
+
+        finalPayment = (numberOfWorkedDays * 8) * payPerHour;
+
+        Console.WriteLine("RESULTADO...");
+        Console.WriteLine("Você receberá {0} por trabalhar por {1} dias recebendo {2} por hora", finalPayment, numberOfWorkedDays, payPerHour);  
+
+
+    }
+}
---
+++ Exercicios/Exercícios Lógica de Programação/ex016/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+16) [DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um 
+fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele 
+já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule 
+quantos dias de vida um fumante perderá e exiba o total em dias.
+*/
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int cigarSmokedPerDay, yearsSmoking;
+        int totalTimeLost, totalDaysLost; 
+
+        Console.Write("Quantos cigarros você fuma por dia? ");
+        cigarSmokedPerDay = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Há quantos anos você fuma? ");
+        yearsSmoking = Convert.ToInt32(Console.ReadLine());
+
+        totalTimeLost = (cigarSmokedPerDay * 10) * (yearsSmoking * 365);
+        totalDaysLost = (totalTimeLost / 60) / 24;
+
+        Console.WriteLine("Por fumar {0} cigarros por dia durante {1} você perdeu {2} dias de vida", cigarSmokedPerDay, yearsSmoking, totalDaysLost); 
+
+    }
+}

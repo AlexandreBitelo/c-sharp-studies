560adb587c04c5a350befb8d7bca5c79a1bea7e0
Alexandre Bitelo <alexandrebitelo41@gmail.com>
2026-01-06 21:52:20 -0300

    exercises 20 and 21

---
+++ Exercicios/Exercícios Lógica de Programação/ex020/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou 
+ÍMPAR. 
+*/
+
+using System; 
+
+class Program
+{
+    static void Main ()
+    {
+        double number = 0;
+        int pairOrOdd;
+
+        Console.WriteLine ("-- VERIFICADOR DE VALORES IMPARES E PARES --");
+
+        Console.Write ("Digite um valor: ");
+        number = Convert.ToDouble(Console.ReadLine());
+
+        pairOrOdd = Convert.ToInt32(number % 2);
+
+        if (pairOrOdd == 0)
+        {
+            Console.WriteLine ("O número {0} é Par", number);
+        } else
+        {
+            Console.WriteLine ("O número {0} é Impar", number); 
+        }
+
+    }
+}
---
+++ Exercicios/Exercícios Lógica de Programação/ex021/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+21) Faça um algoritmo que leia um determinado ano e mostre se ele é ou não 
+BISSEXTO.
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int yearToVerify = 0;
+        bool leapYear = false; 
+
+        Console.WriteLine ("-- VERIFICADOR DE ANO BISSEXTO --");
+
+        Console.Write ("Ano para verificar se é bissexto: ");
+        yearToVerify = Convert.ToInt32(Console.ReadLine());
+        
+        leapYear = (yearToVerify % 4 == 0 ) && (yearToVerify % 100 != 0) || (yearToVerify % 400 == 0); 
+
+        Console.WriteLine ("--------------------VERIFICANDO--------------------------");
+
+        if (leapYear)
+        {
+            Console.WriteLine ("O ano {0} é um ano Bissexto", yearToVerify);
+        } else
+        {
+            Console.WriteLine ("O ano {0} NÃO é um ano bissexto", yearToVerify);
+        }
+
+    }
+}
